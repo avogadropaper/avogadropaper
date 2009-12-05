7672bca3cdf6f0d669ad5bb7b018d007c3fb68aa
Marcus D. Hanwell <marcus@cryos.org>
2009-12-05 16:15:23 -0500

    Committed changes that should have been committed months ago.

---
+++ AvogadroPaper/images/reps_mosaic.sh
@@ -0,0 +1,7 @@
+convert -page +0+0       thiophene-wire.png \
+        -page +0+600     thiophene-stick.png \
+	-page +500+0     thiophene-bs.png \
+	-page +500+600   thiophene-bs-ring.png \
+	-page +1000+0    thiophene-vdw.png \
+	-page +1000+600  thiophene-stick-vdw.png \
+	-mosaic standardReps.png
